27f6ac0f8725c50761c50b3a3b654249c204d8c6
James Hiegel <jhiegel@student.umuc.edu>
2017-01-29 15:24:50 +0100

    initial commit with empty files

---
+++ ThreeAddressGenerator/src/io/github/jameshiegel/ThreeAddGenModel.java
@@ -0,0 +1,5 @@
+package io.github.jameshiegel;
+
+public class ThreeAddGenModel {
+
+}
---
+++ ThreeAddressGenerator/src/io/github/jameshiegel/ThreeAddressGenerator.java
@@ -0,0 +1,10 @@
+package io.github.jameshiegel;
+
+public class ThreeAddressGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
