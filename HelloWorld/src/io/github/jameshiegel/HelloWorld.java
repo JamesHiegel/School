786250ea5812d66af34671616734c24e56cb3eac
[James Hiegel] <[jhiegel@student.umuc.edu]>
2017-02-17 11:47:16 +0100

    test

--- HelloWorld/src/io/github/jameshiegel/HelloWorld.java
+++ HelloWorld/src/io/github/jameshiegel/HelloWorld.java
@@ -3,7 +3,7 @@ package io.github.jameshiegel;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 	}
 
 }
