d9319ad4b20f1dbfdfec7bbcffde8c871e4f6e21
James Hiegel <jhiegel@student.umuc.edu>
2017-07-31 23:22:35 +0200

    started coding the run method

--- CMSC335SeaPortProject2/src/io/github/jameshiegel/Job.java
+++ CMSC335SeaPortProject2/src/io/github/jameshiegel/Job.java
@@ -3,6 +3,8 @@ package io.github.jameshiegel;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.JProgressBar;
+
 //File: Job.java
 //Date: 03 July 2017
 //Author: James Hiegel
@@ -12,15 +14,18 @@ import java.util.Scanner;
  * to complete.
  */
 
-public class Job extends Thing implements Runnable{
+public class Job extends Thing implements Runnable {
 	// instance variables
 	protected double duration = 0.0;
 	protected ArrayList<String> requirements = new ArrayList<String>();
-	
+	private JProgressBar bar;
+	private Thread thread = new Thread();
+
 	// constructor
 	public Job() {
 		super();
 	} // end default constructor
+
 	public Job(Scanner sc) {
 		super(sc);
 		if (sc.hasNextDouble())
@@ -28,25 +33,47 @@ public class Job extends Thing implements Runnable{
 		while (sc.hasNext())
 			requirements.add(sc.next());
 	} // end Scanner constructor
-	
+
 	// methods
 	public double getDuration() {
 		return duration;
 	} // end method getDuration
-	
+
 	public ArrayList<String> getRequirements() {
 		return requirements;
 	} // end method getRequirements
+	
+	public void setThread(Thread thread) {
+		this.thread = thread;
+	} // end method setThread
+	
+	public void setBar(JProgressBar bar) {
+		this.bar = bar;
+	} // end method setBar
 
 	@Override
 	public String toString() {
-		return name;
+		return null;
+		// return name;
+		// return requirements.toString();
 	} // end method toString
-	
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-	
+		int sleepTime = 30;
+		try {
+			if (thread.isAlive())
+				thread.join(); // Will wait on passes thread
+			bar.setString(null); // Removes static text to allow % displayed
+			for (int i = 0; i <= 100; i++) {
+				Thread.sleep(sleepTime);
+				bar.setValue(i);
+			}
+		} catch (InterruptedException e) {
+			bar.setString("Interrupted");
+			return;
+		}
+		bar.setString("Finished");
+	} // end method run
+
 }// end class Job
